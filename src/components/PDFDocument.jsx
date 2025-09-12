02ccfaa7c3fd2270eeef629385a7a1c4ae38d6dc
Affandi99-bit <affandi.blck@gmail.com>
2025-09-12 16:33:08 +0700

    add crew select

--- src/components/PDFDocument.jsx
+++ src/components/PDFDocument.jsx
@@ -452,12 +452,14 @@ const PDFDocument = ({ pro, days }) => {
                     <View style={[styles.tableRow, styles.tableHeader]}>
                       <Text style={styles.tableCellHeader}>Item</Text>
                       <Text style={styles.tableCellHeader}>Qty</Text>
+                      <Text style={styles.tableCellHeader}>Crew</Text>
                       <Text style={styles.tableCellHeader}>Note</Text>
                     </View>
                     {day.expense.orderlist.map((item, idx) => (
                       <View key={idx} style={styles.tableRow}>
                         <Text style={styles.tableCell}>{item.name || "-"}</Text>
                         <Text style={styles.tableCell}>{item.qty || "0"}</Text>
+                        <Text style={styles.tableCell}>{item.crew || "0"}</Text>
                         <Text style={styles.tableCell}>{item.note || "-"}</Text>
                       </View>
                     ))}
